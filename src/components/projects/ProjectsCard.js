1d89ad4efed55bfd5cf2555bf8bf3506d05be179
Rishab Lath <reshablath@gmail.com>
2023-11-08 19:30:08 +0530

    Update ProjectsCard.js

--- src/components/projects/ProjectsCard.js
+++ src/components/projects/ProjectsCard.js
@@ -5,6 +5,7 @@ const ProjectsCard = ({ title, des, src, link }) => {
   return (
     <motion.div 
     initial={{opacity:0.4,y:100}}
+    viewport={{ once: true }}
     transition={{type: "spring",damping:8,stiffness: 80,duration:1}}
     whileInView={{opacity:1,y:0}}
     className="w-full p-4 xl:px-12 h-auto xl:py-10 rounded-lg shadow-shadowOne flex flex-col bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-gray-900 hover:gray-900 transition-colors duration-1000">

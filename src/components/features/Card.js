77d726728b296dfcaa6e2e5c452d58767495f5a7
Rishab Lath <reshablath@gmail.com>
2023-11-08 19:18:40 +0530

    Update Card.js

--- src/components/features/Card.js
+++ src/components/features/Card.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import {motion} from "framer-motion"
 
 const Card = ({title,des,icon}) => {
   return (
-    <div className="w-full px-12 h-80 py-10 rounded-lg shadow-shadowOne flex items-center bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-black hover:to-[#1e2024] transition-colors duration-100 group">
+    <motion.div 
+    initial={{scale:0,opacity:0,rotate:60}}
+    transition={{type: "spring",damping:12,stiffness: 50,duration:0.5}}
+    whileInView={{scale:1,opacity:1,rotate:0}}
+    className="w-full px-12 h-80 py-10 rounded-lg shadow-shadowOne flex items-center bg-gradient-to-r from-bodyColor to-[#202327] group hover:bg-gradient-to-b hover:from-black hover:to-[#1e2024] transition-colors duration-100 group">
       <div className="h-72 overflow-y-hidden">
         <div className="flex h-full flex-col gap-10 translate-y-16 group-hover:translate-y-0 transition-transform duration-500">
           <div className="w-10 h-8 flex flex-col justify-between">
@@ -26,7 +31,7 @@ const Card = ({title,des,icon}) => {
           </div>
         </div>
       </div>
-    </div>
+    </motion.div>
   );
 }
 

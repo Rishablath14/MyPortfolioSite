710e1668b316148dcc8275c7010f8a11ed6d2849
Rishab Lath <reshablath@gmail.com>
2023-12-07 20:08:59 +0530

    Update Navbar.js

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -45,7 +45,7 @@ const Navbar = () => {
         </span>
 <AnimatePresence>
         {showMenu && (
-          <motion.div initital={{x:"-100%"}} transition={{ ease: "easeOut", duration: 1 }} animate={{x:0}} exit={{x:"-100%"}} className="w-[100%] h-screen overflow-scroll absolute top-0 left-[-15px] bg-[#191b1e] p-10 scrollbar-hide">
+          <motion.div initial={{x:"-100%"}} transition={{ ease: "easeOut", duration: 1 }} animate={{x:0}} exit={{x:"-100%"}} className="w-[100%] h-screen overflow-scroll absolute top-0 left-[-15px] bg-[#191b1e] p-10 scrollbar-hide">
             <div className="flex flex-col gap-8 py-2 relative">
               <div>
                 <img className="w-32" src={logo} alt="logo" />

7526a86d13839510a1456307aa7fa792ea168cb9
Rishab Lath <reshablath@gmail.com>
2023-12-21 15:47:14 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,13 @@ import Contact from "./components/contact/Contact";
 import Features from "./components/features/Features";
 import FooterBottom from "./components/footer/FooterBottom";
 import Navbar from "./components/navbar/Navbar";
-import Projects from "./components/projects/Projects";
+// import Projects from "./components/projects/Projects";
 import Resume from "./components/resume/Resume";
 import Preloader from "./components/Preloader";
 
 function App() {
   return (
     <>
-    <Preloader/>
     <div className="w-full h-auto bg-bodyColor text-lightText">
         <Navbar />
       <div className="max-w-screen-xl mx-auto px-4">

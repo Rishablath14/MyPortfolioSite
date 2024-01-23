86fe1857c9f3eb1736034f502ef034d3c54674bb
Rishab Lath <reshablath@gmail.com>
2024-01-24 00:05:46 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -39,7 +39,7 @@ export const metadata = {
   description: "Hyy! I'm Rishab Lath, a freelancer in website development, digital marketing, and Java programming. Explore my work and discover how I can bring your digital projects to life.",
   metadataBase: new URL('https://rishablathportfolio.netlify.app'),
   alternates: {
-    canonical: 'https://rishablathportfolio.netlify.app/',
+    canonical: 'https://rishablathportfolio.netlify.app',
   },
   twitter: {
     card: "Rishab Lath Website",

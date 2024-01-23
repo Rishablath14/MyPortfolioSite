2f3439c451053996c18c52a890273f48cd794114
Rishab Lath <reshablath@gmail.com>
2024-01-23 23:06:24 +0530

    m

---
+++ src/app/assets/index.js
@@ -0,0 +1,23 @@
+import logo from "./images/logo.webp";
+import bannerImg from "./images/bannerImg.webp";
+import projectOne from "./images/dmapinfotech.webp";
+import projectTwo from "./images/ardiginet.webp";
+import projectThree from "./images/GAFNGO.webp";
+import projectFour from "./images/orientaldetective.webp";
+import projectFive from "./images/StrikeMedia.webp";
+import projectSix from "./images/valfom.webp";
+import projectSeven from "./images/Bruorg.webp";
+import contactImg from "./images/contact/contactImg.png";
+
+export {
+  logo,
+  bannerImg,
+  projectOne,
+  projectTwo,
+  projectThree,
+  projectFour,
+  projectFive,
+  projectSix,
+  projectSeven,
+  contactImg,
+};
